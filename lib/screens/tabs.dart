628d14be54f56131228b9a1bbbd1d2a66e48e789
mnengineer <masashiii0115@gmail.com>
2023-08-11 18:56:09 +0700

    feat: お気に入りアイコン動的に変更

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals_app_with_reverpod/providers/favorites_provider.dart';
 import 'package:meals_app_with_reverpod/providers/filters_provider.dart';
-import 'package:meals_app_with_reverpod/providers/meals_provider.dart';
 import 'package:meals_app_with_reverpod/screens/categories.dart';
 import 'package:meals_app_with_reverpod/screens/filters.dart';
 import 'package:meals_app_with_reverpod/screens/meals.dart';
@@ -47,23 +46,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(filterMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
