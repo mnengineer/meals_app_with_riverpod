c0e651543acbea9539256be81534d394345e1bc2
mnengineer <masashiii0115@gmail.com>
2023-08-11 07:43:10 +0700

    feat: StateNotifierProvider作成

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app_with_reverpod/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleMealFavoriteStatus(Meal meal) {
+    final mealsFavorite = state.contains(meal);
+    if (mealsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+    state = [];
+  }
+}
+
+final favoritesMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
